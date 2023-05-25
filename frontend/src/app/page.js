304e241b15276f9fbcb7db380ed000801d3d2d74
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-05-26 00:26:42 +0300

    main-page developing, meterial ui, action buttons

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,91 +1,282 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
 
-export default function Home() {
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+import GroupIcon from "@mui/icons-material/Group";
+import TaskIcon from "@mui/icons-material/Task";
+import CalendarViewMonthIcon from "@mui/icons-material/CalendarViewMonth";
+
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ClearIcon from "@mui/icons-material/Clear";
+import UnpublishedIcon from "@mui/icons-material/Unpublished";
+import EditIcon from "@mui/icons-material/Edit";
+import CheckIcon from "@mui/icons-material/Check";
+
+import {
+  DataGridPremium,
+  GridToolbarQuickFilter,
+  GridToolbarDensitySelector,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+} from "@mui/x-data-grid-premium";
+
+const drawerWidth = 240;
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home(props) {
+  const [rows, setrows] = useState([]);
+  const [selection, setselection] = useState([]);
+
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const CustomToolbar = () => (
+    <GridToolbarContainer>
+      <GridToolbarColumnsButton />
+      <GridToolbarFilterButton />
+      <GridToolbarExport
+        csvOptions={{
+          fileName: "SpartaLogExport_",
+        }}
+      />
+      <GridToolbarDensitySelector />
+      <GridToolbarQuickFilter />
+    </GridToolbarContainer>
+  );
+
+  const [cols] = useState({
+    columns: [
+      {
+        field: "idx",
+        headerName: "S.N.",
+        width: 50,
+        editable: false,
+      },
+      {
+        field: "name",
+        headerName: "Name",
+        width: 125,
+        editable: false,
+      },
+      {
+        field: "email",
+        headerName: "Email",
+        width: 250,
+        editable: false,
+      },
+      {
+        field: "phone",
+        headerName: "Phone",
+        width: 225,
+        editable: false,
+      },
+      {
+        field: "role",
+        headerName: "Role",
+        width: 125,
+        editable: false,
+      },
+      {
+        field: "active",
+        headerName: "Active",
+        width: 125,
+        editable: false,
+      },
+    ],
+  });
+
+  const drawer = (
+    <div>
+      <Toolbar
+        className="font-bold text-2xl max-w-md text-transparent bg-clip-text bg-gradient-to-r from-red-800 to-cyan-700
+            md:text-2xl
+            lg:text-3xl lg:max-w-2xl"
+      >
+        MALWATİON
+      </Toolbar>
+      <Divider />
+      <List>
+        {["Users", "Tasks", "Mail", "Others"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {text == "Users" ? (
+                  <GroupIcon />
+                ) : text == "Tasks" ? (
+                  <TaskIcon />
+                ) : text == "Mail" ? (
+                  <MailIcon />
+                ) : (
+                  <CalendarViewMonthIcon />
+                )}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["Tasks", "Mail", "Others"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="h-[85vh]">
+      <Box className="h-full" sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
+        >
+          <Toolbar className="bg-[#f0f2f5]">
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              className="text-transparent bg-clip-text bg-gradient-to-r from-black to-cyan-700 font-bold"
+              variant="h6"
+              noWrap
+              component="div"
+            >
+              Users
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true,
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: "none", sm: "block" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box
+          className="h-full"
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+          }}
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <Toolbar />
+          <div className="mt-2 mb-3  flex justify-end">
+            <button className="btn bg-green-500/50 hover:bg-green-500">
+              <CheckIcon className="text-white" />
+            </button>
+            <button className="btn bg-orange-400/50 hover:bg-orange-400">
+              <ClearIcon className="text-white" />
+            </button>
+            <button className="btn bg-blue-600/50 hover:bg-blue-600">
+              <EditIcon className="text-white" />
+            </button>
+            <button className="btn bg-red-700/50 hover:bg-red-700">
+              <DeleteForeverIcon className="text-white" />
+            </button>
+          </div>
+          <DataGridPremium
+            className="h-full"
+            getDetailPanelContent={({ row }) => (
+              <div className="master-container">
+                <p className="font-bold">hello master info</p>
+              </div>
+            )}
+            getDetailPanelHeight={({ row }) => 500} // Optional, default is 500px
+            {...cols}
+            {...{ rows: [...rows] }}
+            checkboxSelection
+            components={{
+              Toolbar: CustomToolbar,
+            }}
+            onRowSelectionModelChange={(newSelection) => {
+              console.log(newSelection);
+              setselection(newSelection);
+            }}
+            selectionModel={selection}
+          />
+        </Box>
+      </Box>
     </main>
-  )
+  );
 }
